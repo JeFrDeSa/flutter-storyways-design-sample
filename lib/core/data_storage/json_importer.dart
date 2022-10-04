62437f42555745692ece44ca432cd2af114d4aa9
JFDS <JFDS@Project01>
2022-10-04 13:33:02 +0200

    Change month of date as in layout (01. -> Jan)

--- lib/core/data_storage/json_importer.dart
+++ lib/core/data_storage/json_importer.dart
@@ -6,18 +6,18 @@ class JsonImporter {
 
   @visibleForTesting
   List<Map<String, dynamic>> allBooks = [
-    {"title":"The Island","author":"Sam Samko","continueReading":"false","releaseDate": "03.02.2022","coverImagePath": "assets/images/covers/island.jpg"},
-    {"title":"The Computer","author":"Sam Samko","continueReading":"false","releaseDate": "20.11.2011","coverImagePath": "assets/images/covers/computer.jpg"},
-    {"title":"The Ocean","author":"Samenta Bix","continueReading":"false","releaseDate": "01.05.2011","coverImagePath": "assets/images/covers/wave.jpg"},
-    {"title":"The Nature","author":"Lisa Pisichen","continueReading":"false","releaseDate": "03.02.2022","coverImagePath": "assets/images/covers/nature.jpg"},
-    {"title":"Paddling","author":"Lisa Pisichen","continueReading":"false","releaseDate": "20.11.2011","coverImagePath": "assets/images/covers/paddling.jpg"},
-    {"title":"Sunrise","author":"Chris Blue","continueReading":"false","releaseDate": "01.05.2011","coverImagePath": "assets/images/covers/sunrise.jpg"},
-    {"title":"The Typewriter","author":"Sam Samko","continueReading":"false","releaseDate": "03.02.2022","coverImagePath": "assets/images/covers/typewriter.jpg"},
-    {"title":"Utilities","author":"Thomas Key","continueReading":"false","releaseDate": "20.11.2011","coverImagePath": "assets/images/covers/utilities.jpg"},
-    {"title":"The Club","author":"Susan Moller","continueReading":"true","releaseDate": "05.12.2012","coverImagePath": "assets/images/covers/club.jpg"},
-    {"title":"The Fork","author":"Susan Moller","continueReading":"true","releaseDate": "11.03.2011","coverImagePath": "assets/images/covers/fork.jpg"},
-    {"title":"The Mountain","author":"Samenta Bix","continueReading":"true","releaseDate": "01.03.2011","coverImagePath": "assets/images/covers/mountain.jpg"},
-    {"title":"The Typewriter","author":"Sam Samko","continueReading":"true","releaseDate": "03.02.2022","coverImagePath": "assets/images/covers/typewriter.jpg"},
+    {"title":"The Island","author":"Sam Samko","continueReading":"false","releaseDate": "03. Jan 2022","coverImagePath": "assets/images/covers/island.jpg"},
+    {"title":"The Computer","author":"Sam Samko","continueReading":"false","releaseDate": "20. Jan 2011","coverImagePath": "assets/images/covers/computer.jpg"},
+    {"title":"The Ocean","author":"Samenta Bix","continueReading":"false","releaseDate": "01. Dez 2011","coverImagePath": "assets/images/covers/wave.jpg"},
+    {"title":"The Nature","author":"Lisa Pisichen","continueReading":"false","releaseDate": "03. Jan 2022","coverImagePath": "assets/images/covers/nature.jpg"},
+    {"title":"Paddling","author":"Lisa Pisichen","continueReading":"false","releaseDate": "20. Dez 2011","coverImagePath": "assets/images/covers/paddling.jpg"},
+    {"title":"Sunrise","author":"Chris Blue","continueReading":"false","releaseDate": "01. Dez 2011","coverImagePath": "assets/images/covers/sunrise.jpg"},
+    {"title":"The Typewriter","author":"Sam Samko","continueReading":"false","releaseDate": "03. Feb 2022","coverImagePath": "assets/images/covers/typewriter.jpg"},
+    {"title":"Utilities","author":"Thomas Key","continueReading":"false","releaseDate": "20. Jan 2011","coverImagePath": "assets/images/covers/utilities.jpg"},
+    {"title":"The Club","author":"Susan Moller","continueReading":"true","releaseDate": "05. Feb 2012","coverImagePath": "assets/images/covers/club.jpg"},
+    {"title":"The Fork","author":"Susan Moller","continueReading":"true","releaseDate": "11. Dez 2011","coverImagePath": "assets/images/covers/fork.jpg"},
+    {"title":"The Mountain","author":"Samenta Bix","continueReading":"true","releaseDate": "01. Feb 2011","coverImagePath": "assets/images/covers/mountain.jpg"},
+    {"title":"The Typewriter","author":"Sam Samko","continueReading":"true","releaseDate": "03. Jan 2022","coverImagePath": "assets/images/covers/typewriter.jpg"},
   ];
 
   /// Returns the local stored books.
