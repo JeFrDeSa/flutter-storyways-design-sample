b62c8e5c4856077c48dd03d99c2ce2c8fd34ff41
JFDS <JFDS@Project01>
2022-10-05 13:51:41 +0200

    Refactor search result to use determineListEntryKey

--- test/widget_tests/feature/view/widgets/search_result_entry_test.dart
+++ test/widget_tests/feature/view/widgets/search_result_entry_test.dart
@@ -1,3 +1,4 @@
+import 'package:design_sample/core/utilities/constants.dart' as constants;
 import 'package:design_sample/feature/view/widgets/search_result_entry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -6,28 +7,31 @@ import '../../../../test_utilities/fixtures/book_fixtures.dart' as book_fixtures
 import '../../../../test_utilities/fixtures/widget_test_container_fixtures.dart' as widget_test_container_fixtures;
 
 void main() {
+  const int index = 0;
+
   Widget widgetTestContainer = widget_test_container_fixtures.getDefaultWidgetTestContainer(SearchResultEntry(
     backgroundImage: AssetImage(book_fixtures.theComputer.coverImagePath),
     title: book_fixtures.theComputer.title,
     author: book_fixtures.theComputer.author,
+    listIndex: index,
   ));
 
   group('Layout representation', () {
-    testWidgets('should show the predefined layout when created.', (WidgetTester tester) async {
+    testWidgets('should show the book information and cover image when created.', (WidgetTester tester) async {
       // (A)rrange -> all necessary preconditions and inputs.
+      ValueKey key = constants.determineListEntryKey(key: constants.searchResultEntryImageKey, index: index);
       await tester.pumpWidget(widgetTestContainer);
+      await tester.pump();
 
       // (A)ct -> on the object or method under test.
-      final playButton = find.byType(TextButton);
-      final titleText = find.text(book_fixtures.theComputer.title);
-      final authorText = find.text(book_fixtures.theComputer.author);
-      final coverImage = find.image(AssetImage(book_fixtures.theComputer.coverImagePath));
+      final titleAndAuthor = tester.widget<RichText>(find.byType(RichText)).text.toPlainText();
+      final container = tester.widget<Container>(find.byKey(key));
+      String boxDecorationProperties = (container.decoration! as BoxDecoration).toString();
 
       // (A)ssert -> that the expected results have occurred.
-      expect(playButton, findsOneWidget);
-      expect(titleText, findsOneWidget);
-      expect(authorText, findsOneWidget);
-      expect(coverImage, findsOneWidget);
+      expect(titleAndAuthor.contains(book_fixtures.theComputer.title), true);
+      expect(titleAndAuthor.contains(book_fixtures.theComputer.author), true);
+      expect(boxDecorationProperties.contains(book_fixtures.theComputer.coverImagePath), true);
     });
   });
 }
--- test/widget_tests/feature/view/widgets/search_result_test.dart
+++ test/widget_tests/feature/view/widgets/search_result_test.dart
@@ -1,33 +1,59 @@
+import 'package:design_sample/core/utilities/constants.dart' as constants;
+import 'package:design_sample/feature/controller/book_search.dart';
 import 'package:design_sample/feature/view/widgets/search_result.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../test_utilities/fixtures/widget_test_container_fixtures.dart' as widget_test_container_fixtures;
 import '../../../../test_utilities/fixtures/book_fixtures.dart' as book_fixtures;
+import '../../../../test_utilities/fixtures/widget_test_container_fixtures.dart' as widget_test_container;
 
 void main() {
-  Widget widgetTestContainer = widget_test_container_fixtures.getDefaultWidgetTestContainer(
-    const ProviderScope(
-      child: SearchResult(),
-    ),
-  );
+  const int index = 0;
+
+  Widget getBookSearchConsumerWidgetTestContainer() {
+    return Consumer(builder: (context, ref, _) {
+      return Column(
+        children: <Widget>[
+          MaterialButton(
+            child: const SizedBox(),
+            onPressed: () {
+              ref.read(searchProvider.state).update((state) => "COMPUTER");
+            },
+          ),
+          const Expanded(
+            child: SearchResult(),
+          ),
+        ],
+      );
+    });
+  }
+
+  Widget getProviderScope() {
+    return ProviderScope(
+      child: widget_test_container.getDefaultWidgetTestContainer(getBookSearchConsumerWidgetTestContainer()),
+    );
+  }
 
   group('Layout representation', () {
-    testWidgets('should show the predefined layout when created.', (WidgetTester tester) async {
+    testWidgets('should show the search result as a list of books when created.', (WidgetTester tester) async {
       // (A)rrange -> all necessary preconditions and inputs.
-      await tester.pumpWidget(widgetTestContainer);
+      ValueKey key = constants.determineListEntryKey(key: constants.searchResultEntryImageKey, index: index);
+      await tester.pumpWidget(getProviderScope());
       await tester.pump();
 
       // (A)ct -> on the object or method under test.
-      final coverImage = find.byType(TextField);
-      final titleText = find.text(book_fixtures.theComputer.title);
-      final authorText = find.text(book_fixtures.theComputer.author);
+      await tester.tap(find.byType(MaterialButton));
+      await tester.pumpAndSettle();
+
+      final titleAndAuthor = tester.widget<RichText>(find.byType(RichText)).text.toPlainText();
+      final container = tester.widget<Container>(find.byKey(key));
+      String boxDecorationProperties = (container.decoration! as BoxDecoration).toString();
 
       // (A)ssert -> that the expected results have occurred.
-      expect(coverImage, findsOneWidget);
-      expect(titleText, findsOneWidget);
-      expect(authorText, findsOneWidget);
+      expect(titleAndAuthor.contains(book_fixtures.theComputer.title), true);
+      expect(titleAndAuthor.contains(book_fixtures.theComputer.author), true);
+      expect(boxDecorationProperties.contains(book_fixtures.theComputer.coverImagePath), true);
     });
   });
 }
